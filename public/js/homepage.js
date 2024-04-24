e203f11153cfd9c19dae7d8a4a9f63c0ddde109b
Aiden Wahed <prismhead26@gmail.com>
2024-04-24 14:45:34 -0600

    heroku

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -3,12 +3,12 @@ const createComment = async (event) => {
     event.preventDefault();
   if( event.target.matches('button') ) {
     
-    console.dir(event.target);
+    // console.dir(event.target);
 
     const description = event.target.parentElement[0].value.trim()
     const blog_id = event.target.parentElement[0].dataset.id
 
-    console.log(description, blog_id)
+    // console.log(description, blog_id)
 
     if (description && blog_id) {
         const response = await fetch(`api/comments`, {
@@ -28,7 +28,7 @@ const createComment = async (event) => {
 
   const commentEl = document
     .querySelectorAll(".comment-forum")
-  console.log(commentEl)
+  // console.log(commentEl)
 commentEl.forEach(comment => {
   comment.addEventListener("click", createComment);
 });
